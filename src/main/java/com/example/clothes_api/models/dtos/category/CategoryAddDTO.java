06c6cedd26be3d50d24a524b61ef8cb9f4932047
GabrielSantosMedeiros <gabriel7.gs61@gmail.com>
2024-08-06 09:46:33 -0300

    add function in category service

--- src/main/java/com/example/clothes_api/models/dtos/category/CategoryAddDTO.java
+++ src/main/java/com/example/clothes_api/models/dtos/category/CategoryAddDTO.java
@@ -1,6 +1,5 @@
 package com.example.clothes_api.models.dtos.category;
 
 public record CategoryAddDTO(
-    String id,
     String name
 ) {}
--- src/main/java/com/example/clothes_api/services/CategoryService.java
+++ src/main/java/com/example/clothes_api/services/CategoryService.java
@@ -1,15 +1,29 @@
 package com.example.clothes_api.services;
 
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.example.clothes_api.models.Category;
+import com.example.clothes_api.models.dtos.category.CategoryAddDTO;
+import com.example.clothes_api.models.dtos.category.CategoryViewDTO;
 import com.example.clothes_api.repositores.CategoryRepository;
 
 @Service
 public class CategoryService {
     
     @Autowired
-    private static CategoryRepository CategoryRepository;
+    private static CategoryRepository categoryRepository;
 
-
+    public CategoryViewDTO addCategory(CategoryAddDTO dto) {
+        Category category = new Category();
+        BeanUtils.copyProperties(dto, category);
+        categoryRepository.save(category);
+        return new CategoryViewDTO(
+            category.getId(),
+            category.getName(),
+            category.getCreatedAt(),
+            category.getUpdatedAt()
+        );
+    }
 }
